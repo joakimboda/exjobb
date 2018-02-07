62f274b6def1fc40c6a633fc76a1d41111602d66
Joakim Boda (White) <joakim2_92@hotmail.com>
2018-02-08 00:19:38 +0100

    interface calc added

--- exjobb/interface_calc.py
+++ exjobb/interface_calc.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[42]:
+# In[38]:
 
 
 import Bio
@@ -15,13 +15,23 @@ import numpy as np
 import os
 
 import pandas
+import multiprocessing
 
 
-# In[43]:
+#This is for removing Bio.python warnings
+import warnings
+from Bio import BiopythonWarning
+warnings.simplefilter('ignore', BiopythonWarning)
+
+
+# In[39]:
 
 
 def calc_residue_dist(residue1, residue2,layers) :
     """Returns the C-alpha distance between two residues"""
+    layers_VDW=[]
+    layers_CA=[]
+    layers_HA=[]
     for atom1 in residue1:
         for atom2 in residue2:
             
@@ -32,17 +42,20 @@ def calc_residue_dist(residue1, residue2,layers) :
             if distance >8:#Go to the next atom
                 continue
             if distance<=0.5:
-                layers.setdefault('VDW', []).append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
+                layers_VDW.append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
+                #layers.setdefault('VDW', []).append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
 
                 
             if 'H' not in atom1.get_id() and 'H' not in atom2.get_id(): # DUBBELKOLLA ATT DET INTE FINNS H I ANDRA!!!!
             #if atom1.get_id() != 'H' and atom2.get_id() != 'H':    
                 if atom1.get_name()=='CA' and atom2.get_name()=='CA' and distance<=8: #CA-CA distance
-                    layers.setdefault('CA-CA', []).append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
+                    layers_CA.append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
+                    #layers.setdefault('CA-CA', []).append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
                 if distance<=5: #Heavy atoms distance
-                    layers.setdefault('H-A', []).append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
-    return (layers)
-
+                    layers_HA.append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
+                    #layers.setdefault('H-A', []).append([str(residue1.get_full_id()[3][1]) +'.'+ residue1.get_resname() +'-'+ str(residue2.get_full_id()[3][1]) +'.'+ residue2.get_resname(),atom1.get_name() +'-'+ atom2.get_name(), distance])
+    #return (layers)
+    return ([layers_VDW,layers_CA,layers_HA])
 
 
 def calc_dist_matrix(chain_one, chain_two,layers) :
@@ -50,11 +63,12 @@ def calc_dist_matrix(chain_one, chain_two,layers) :
     answer = np.zeros((len(chain_one), len(chain_two)), np.float)
     for row, residue_one in enumerate(chain_one) :
         for col, residue_two in enumerate(chain_two) :
-            layers = calc_residue_dist(residue_one, residue_two,layers)
+            layers.append(calc_residue_dist(residue_one, residue_two,layers))
+            
     return (layers)
 
 
-# In[44]:
+# In[40]:
 
 
 def find_protein_value(pdb_name,value_file):
@@ -62,39 +76,43 @@ def find_protein_value(pdb_name,value_file):
     #Re.search(r”([\w.]+)@([\w.]+)”, value_file) 
 
 
-# In[45]:
+# In[41]:
 
 
 def main():
     
     #Dir path
     #args = sys.argv[1:]
-    args='/home/joakim/Downloads/models' #str(args[0])
+    args='D:\Downloads\CnM-dataset\models' #'/home/joakim/Downloads/models' #str(args[0])
     
     pdb_list=[]
     for file in os.listdir(args):
         if file.endswith(".pdb"):
-            pdb_list.append(args+'/'+file)
-    print pdb_list
+            #pdb_list.append(args+'/'+file)
+            pdb_list.append([args,file])
+    #print pdb_list
     #filename_pdb = '/home/joakim/Downloads/*.pdb'#'/home/joakim/Downloads/2HIY_A.pdb' #'/home/joakim/Downloads/D1A2K-a0a-merged.pdb'
     
+    valuefile='D:\Downloads\CnM.featuresNPqDNZ'#'/home/joakim/Downloads/CnM.featuresNPqDNZ'
+    value_df=pandas.read_csv(valuefile,delim_whitespace=1)
     
-    value_df=pandas.read_csv('/home/joakim/Downloads/CnM.featuresNPqDNZ',delim_whitespace=1)
-    print(value_df)
     
-    value_file=open('/home/joakim/Downloads/CnM.featuresNPqDNZ', 'r').read().splitlines()
+    
+    #value_file=open('/home/joakim/Downloads/CnM.featuresNPqDNZ', 'r').read().splitlines()
     #print(value_file)
     
-    
-    for counter, filename_pdb in enumerate(pdb_list):
-        print counter
-        
+    score=[]
+    dist_matrix=[]
+    for counter, filepath_pdb in enumerate(pdb_list):
+        print(counter)
+        filename_pdb=os.path.join(pdb_list[counter][0],pdb_list[counter][1])
+
         try: 
             PDBobj = PDBParser()
             structure = PDBobj.get_structure(filename_pdb, filename_pdb)
             model = structure[0]
         except IOError: 
-            print 'IO Error', filename_pdb       
+            print('IO Error', filename_pdb)      
             while 'true':
                 input1=raw_input("Error parsing PDB file! Continue(y/n)...")
                 if input1.lower()=='y':
@@ -102,18 +120,35 @@ def main():
                 elif input1.lower()=='n':
                     sys.exit()
 
+        
+        score.append(value_df.loc[((value_df['#'] == pdb_list[counter][1][:-11]),'CPscore')].values[0])
+                    
         chain_used=[]
-        layers={}
-        layers.setdefault('VDW', []).append(['Check'])
-        layers.setdefault('CA-CA', []).append(['Check'])
-        layers.setdefault('H-A', []).append(['Check'])
+        #layers={}
+        #layers.setdefault('VDW', []).append(['Check'])
+        #layers.setdefault('CA-CA', []).append(['Check'])
+        #layers.setdefault('H-A', []).append(['Check'])
         for chain1 in model:
             for chain2 in model:
                 if chain1!=chain2 and chain2 not in chain_used:
                     chain_used.append(chain1)
-                    layers = calc_dist_matrix(chain1, chain2,layers)
+                    layers=[]
+                    #layers = calc_dist_matrix(chain1, chain2,layers)
+                    p = multiprocessing.Process(target=dist_matrix.append)
+                    p.start()
+                    #dist_matrix.append(calc_dist_matrix(chain1, chain2,layers))
                     #contact_map = dist_matrix < 12.0
 
+    print('hej')
+    #for counter,i in
+    score=np.array(score)
+    
+    
+    print(score[1])
+    print(score.shape)
+    #dist_matrix=np.array(dist_matrix)
+    #print(dist_matrix.shape)
+    
      #   t=0
      #   for i in layers['CA-CA']:
        #     t=t+1
@@ -137,5 +172,5 @@ def main():
 
     
 if __name__ == '__main__':
-  main()
+    main()
 
